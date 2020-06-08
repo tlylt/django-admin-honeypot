c01eef8db1abe077f3a096fb8f5b12b82ef9a8c1
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2020-06-09 00:16:32 +0800

    Update listeners.py

--- admin_honeypot/listeners.py
+++ admin_honeypot/listeners.py
@@ -4,8 +4,11 @@ from django.core.mail import mail_admins
 from django.template.loader import render_to_string
 from django.urls import reverse
 
-
+# this is a receiver function, the honeypot signal sends two variables: instance, request
 def notify_admins(instance, request, **kwargs):
+    '''
+    gathers details in context, render email through the template and send through django mail helper
+    '''
     path = reverse('admin:admin_honeypot_loginattempt_change', args=(instance.pk,))
     admin_detail_url = 'http://{0}{1}'.format(request.get_host(), path)
     context = {
@@ -17,5 +20,6 @@ def notify_admins(instance, request, **kwargs):
     message = render_to_string('admin_honeypot/email_message.txt', context).strip()
     mail_admins(subject=subject, message=message)
 
+# check attribute of settings, if True, register signal to receiver callback function (notify_admins)
 if getattr(settings, 'ADMIN_HONEYPOT_EMAIL_ADMINS', True):
     honeypot.connect(notify_admins)
